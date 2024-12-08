10f6040ee0f68fc84a454e2d3bab68c1c4bbd80b
agras7 <vassilis.agrafiotis@gmail.com>
2024-12-08 18:31:50 +0100

    Day 8, Part 2

--- day_8.py
+++ day_8.py
@@ -1,6 +1,27 @@
 import time
 import os
 
+moves = {
+    "u-l": (-1, -1),
+    "d-r": (1, 1),
+    "u-r": (-1, 1),
+    "d-l": (1, -1)
+}
+
+def search_antinodes(start_pos, dif_x, dif_y, direction, x_limit, y_limit, nodes):
+    move = moves[direction]
+    cur_x = start_pos[0]
+    cur_y = start_pos[1]
+    while cur_x in range(0, x_limit) and cur_y in range(0, y_limit):
+        nodes.add((cur_x, cur_y))
+        next_x = cur_x + move[0] * abs(dif_x)
+        next_y = cur_y + move[1] * abs(dif_y)
+        if next_x in range(0, x_limit) and next_y in range(0, y_limit):
+            #print("Add")
+            nodes.add((next_x, next_y))
+        cur_x, cur_y = next_x, next_y
+    return
+
 start = time.time()
 file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"inputs\Day 8"), 'r')
 
@@ -46,7 +67,6 @@ print("Part 1 result: {}".format(len(antinodes)))
 print("The time of execution of above program is :", (end - start) * 10 ** 3, "ms")
 print()
 
-start = time.time()
 antinodes_2 = set()
 for t_antenna in antennas.keys():
     for i, a in enumerate(antennas[t_antenna]):
@@ -54,66 +74,12 @@ for t_antenna in antennas.keys():
             dx = a[0] - _a[0]
             dy = a[1] - _a[1]
 
-            # dy > 0 indicates a positive slope for the line linking the two points
             if dy > 0:
-                # Searching for a possible anti-node up-right
-                antinodes_2.add((a[0], a[1]))
-                current_x = a[0]
-                current_y = a[1]
-                new_x = a[0] - abs(dx)
-                new_y = a[1] + abs(dy)
-                while current_x in range(0, x_len) and current_y in range(0, y_len):
-                    if new_x in range(0, x_len) and new_y in range(0, y_len):
-                        antinodes_2.add((new_x, new_y))
-                    current_x = new_x
-                    current_y = new_y
-                    new_x -= abs(dx)
-                    new_y += abs(dy)
-
-                # Searching for a possible anti-node down-left
-                antinodes_2.add((_a[0], _a[1]))
-                current_x = _a[0]
-                current_y = _a[1]
-                new_x = _a[0] + abs(dx)
-                new_y = _a[1] - abs(dy)
-                while current_x in range(0, x_len) and current_y in range(0, y_len):
-                    if new_x in range(0, x_len) and new_y in range(0, y_len):
-                        antinodes_2.add((new_x, new_y))
-                    current_x = new_x
-                    current_y = new_y
-                    new_x += abs(dx)
-                    new_y -= abs(dy)
-
-            # dy < 0 indicates a negative slope for the line linking the two points
+                search_antinodes(a, dx, dy, "u-r", x_len, y_len, antinodes_2)
+                search_antinodes(_a, dx, dy, "d-l", x_len, y_len, antinodes_2)
             else:
-                # Searching for a possible anti-node up-left
-                antinodes_2.add((a[0], a[1]))
-                current_x = a[0]
-                current_y = a[1]
-                new_x = a[0] - abs(dx)
-                new_y = a[1] - abs(dy)
-                while current_x in range(0, x_len) and current_y in range(0, y_len):
-                    #print("Going up-left")
-                    if new_x in range(0, x_len) and new_y in range(0, y_len):
-                        antinodes_2.add((new_x, new_y))
-                    current_x = new_x
-                    current_y = new_y
-                    new_x -= abs(dx)
-                    new_y -= abs(dy)
-
-                # Searching for a possible anti-node down-right
-                antinodes_2.add((_a[0], _a[1]))
-                current_x = _a[0]
-                current_y = _a[1]
-                new_x = _a[0] + abs(dx)
-                new_y = _a[1] + abs(dy)
-                while current_x in range(0, x_len) and current_y in range(0, y_len):
-                    if new_x in range(0, x_len) and new_y in range(0, y_len):
-                        antinodes_2.add((new_x, new_y))
-                    current_x = new_x
-                    current_y = new_y
-                    new_x += abs(dx)
-                    new_y += abs(dy)
+                search_antinodes(a, dx, dy, "u-l", x_len, y_len, antinodes_2)
+                search_antinodes(_a, dx, dy, "d-r", x_len, y_len, antinodes_2)
 
 end = time.time()
 print("Part 2 result: {}".format(len(antinodes.union(antinodes_2))))
