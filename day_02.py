fc145101d355a74033a90fea68bd4e8231013b07
agras7 <vassilis.agrafiotis@gmail.com>
2024-12-14 20:02:24 +0100

    Day 2, Part 2

--- day_02.py
+++ day_02.py
@@ -3,12 +3,12 @@ from collections import Counter
 import time
 import os
 
-def is_report_valid(report) -> bool:
+def is_report_valid(_report) -> bool:
     diff = []
     diff_abs = []
-    for i in range(0, len(report) - 1):
-        diff.append(report[i] - report[i+1])
-        diff_abs.append(abs(report[i] - report[i+1]))
+    for i in range(0, len(_report) - 1):
+        diff.append(_report[i] - _report[i + 1])
+        diff_abs.append(abs(_report[i] - _report[i + 1]))
 
     if all(val > 0 for val in diff):
         if all(val <= 3 for val in diff_abs):
@@ -18,49 +18,37 @@ def is_report_valid(report) -> bool:
             return True
     return False
 
+start = time.time()
 file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"inputs\Day 02"), 'r')
 
-
-start_p1 = time.time()
 counter = 0
 for l in file:
     report = list(map(int, l.split()))
     if is_report_valid(report):
         counter += 1
 
-end_p1 = time.time()
+end = time.time()
 print("Part 1 result: {}".format(counter))
-print("The time of execution of above program is :", (end_p1-start_p1) * 10**3, "ms")
+print("The time of execution of above program is :", (end-start) * 10**3, "ms")
 print()
-'''
 
-def is_report_valid_part_2(arr) -> bool:
-    diff = []
-    diff_abs = []
-    for i in range(0, len(arr) - 1):
-        diff.append(arr[i] - arr[i + 1])
-        diff_abs.append(abs(arr[i] - arr[i + 1]))
+def is_report_valid_part_2(_report) -> bool:
+    for i in range(len(_report)):
+        # Remove one level and check if the new report is now safe
+        modified_report = _report[:i] + _report[i + 1:]
+        if is_report_valid(modified_report):
+            return True
+    return False
 
-    print(report)
-    print(diff)
-    print(diff_abs)
-    if len(list(filter(lambda x: x <= 0, diff))) > 1:
-        return False
-    if len(list(filter(lambda x: x > 3, diff_abs))) > 1:
-        return False
-
-    return True
-
-start_p2 = time.time()
-counter2 = 0
+start = time.time()
+file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"inputs\Day 02"), 'r')
+counter_2 = 0
 for l in file:
     report = list(map(int, l.split()))
     if is_report_valid_part_2(report):
-        counter2 += 1
-        print("Safe")
-        print()
+        counter_2 += 1
 
-end_p2 = time.time()
-print("Part 2 result: {}".format(counter2))
-print("The time of execution of above program is :", (end_p2 - start_p2) * 10**3, "ms")
-print()'''
+end = time.time()
+print("Part 2 result: {}".format(counter_2))
+print("The time of execution of above program is :", (end - start) * 10 ** 3, "ms")
+print()
